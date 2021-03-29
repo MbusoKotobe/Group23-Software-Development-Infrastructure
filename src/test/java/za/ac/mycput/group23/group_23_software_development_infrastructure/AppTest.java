4345c561951de2e233b00e547405588b04e48bf2
MbusoKotobeWrk <mbusokotobewrk@gmail.com>
2021-03-30 01:39:54 +0200

    Initial Commit

---
+++ src/test/java/za/ac/mycput/group23/group_23_software_development_infrastructure/AppTest.java
@@ -0,0 +1,20 @@
+package za.ac.mycput.group23.group_23_software_development_infrastructure;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
