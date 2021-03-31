f459687e5bfc259b85b9527197a086d3f4d9a858
MbusoKotobe <mbuso.kotobes@gmail.com>
2021-03-31 13:47:30 +0200

    Project Cleanup

--- src/main/java/za/ac/mycput/group23/group_23_software_development_infrastructure/App.java
+++ src/main/java/za/ac/mycput/group23/group_23_software_development_infrastructure/App.java
@@ -1,19 +1,9 @@
 package za.ac.mycput.group23.group_23_software_development_infrastructure;
 
-import java.util.regex.MatchResult;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import jdk.nashorn.internal.runtime.regexp.RegExp;
 
-/**
- * @author MbusoKotobe 218040385
- * <br/>
- * This small program is a password checker & verifier. 
- */
 public class App 
 {   
-
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
