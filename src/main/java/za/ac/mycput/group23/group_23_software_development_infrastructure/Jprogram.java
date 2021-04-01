94898bfd62d1a80be50c49529cf6a583a6fe188e
Juan-Lee <klink.juan@gmail.com>
2021-04-01 13:09:06 +0200

    4th commit

--- src/main/java/za/ac/mycput/group23/group_23_software_development_infrastructure/Jprogram.java
+++ src/main/java/za/ac/mycput/group23/group_23_software_development_infrastructure/Jprogram.java
@@ -7,8 +7,26 @@ package za.ac.mycput.group23.group_23_software_development_infrastructure;
 
 /**
  *
- * @author Juan
+ * @author Juan-Lee Klink
+ * code to make a human 
  */
 public class Jprogram {
+    
+    private String humanName;
+    private String humanGender;
+    private String humanFavouritefood;
+    private String humanFavouriteanime;
+    
+    public Jprogram(){
+        System.out.println("In Jprogram constructor");
+    }
+    
+    public Jprogram(String hName,String hGender,String hFavfood,String hFavanime){
+        this.humanName = hName;
+        this.humanGender = hGender;
+        this.humanFavouritefood = hFavfood;
+        this.humanFavouriteanime = hFavanime;
+    }
+    
     
 }
