a7cc47c5c0d074ff060378b17dd4d709ffa7c2e7
Juan-Lee <klink.juan@gmail.com>
2021-04-01 12:22:46 +0200

    2nd commit

--- src/main/java/za/ac/mycput/group23/group_23_software_development_infrastructure/Jprogram.java
+++ src/main/java/za/ac/mycput/group23/group_23_software_development_infrastructure/Jprogram.java
@@ -4,7 +4,7 @@ package za.ac.mycput.group23.group_23_software_development_infrastructure;
  * Hello world!
  *
  */
-public class App 
+public class Jprogram 
 {
     public static void main( String[] args )
     {
