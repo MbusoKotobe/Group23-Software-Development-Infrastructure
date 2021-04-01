bb05b5460442290fea1e8a98ec5d662ef8798530
Juan-Lee <klink.juan@gmail.com>
2021-04-01 12:29:47 +0200

    3rd commit added a java class

--- src/main/java/za/ac/mycput/group23/group_23_software_development_infrastructure/Jprogram.java
+++ src/main/java/za/ac/mycput/group23/group_23_software_development_infrastructure/Jprogram.java
@@ -1,13 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package za.ac.mycput.group23.group_23_software_development_infrastructure;
 
 /**
- * Hello world!
  *
+ * @author Juan
  */
-public class Jprogram 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class Jprogram {
+    
 }
---
+++ src/main/java/za/ac/mycput/group23/group_23_software_development_infrastructure/RunJprogram.java
@@ -0,0 +1,13 @@
+package za.ac.mycput.group23.group_23_software_development_infrastructure;
+
+/**
+ * Hello world!
+ *
+ */
+public class RunJprogram 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
