4352106f2ea4c200664b06afa93deed991fed0a7
Mbuso Kotobe <mbuso.kotobes@gmail.com>
2023-03-11 23:34:58 +0200

    Saving my changes

---
+++ src/main/java/za/ac/mycput/group23/group_23_software_development_infrastructure/YourPojoClass.java
@@ -0,0 +1,7 @@
+package za.ac.mycput.group23.group_23_software_development_infrastructure;
+
+public class YourPojoClass {
+    //Code your Pojo Class
+
+    //Instructions or Reminder is in the project I am going to send you.
+}
